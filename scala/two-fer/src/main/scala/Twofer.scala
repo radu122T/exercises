fa024c70467d9c90a91bdf3e572ce65e8d8b30a9
raducudalb <radu.cudalb@veridion.com>
2025-04-07 19:13:21 +0300

    finished twofer

---
+++ scala/two-fer/src/main/scala/Twofer.scala
@@ -0,0 +1,8 @@
+object Twofer {
+  def twofer(name: String): String = {
+    if (Option(name).exists(_.nonEmpty))
+      s"One for $name, one for me."
+    else
+      "One for you, one for me."
+  }
+}
